8b0f511998b716a031c4fa8826b972d59185728f
JamieKessler <jkessler5535@gmail.com>
2023-11-29 18:34:40 -0800

    added styles to .content and added media queries

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -6,25 +6,30 @@ const Home = () => {
   return (
     <div className="home-page">
       <div className="hero">
-       <div className="hero-img-container" style={{ backgroundColor: "#ced4da"}}>
-        <img src={coffeeImage} alt="coffee/kamal-preet" className="hero-img" />
-       </div>
-        <div className="content">
-            <div className="inner-content">
-            <h1>Welcome to my blog</h1>
-              <p>
-                Hi! Welcome to the most trusted online source for naturally minded moms… we’re so glad you’re here. We believe
-                that moms are the most powerful force on the planet and we’re unwavering in supporting, uplifting, and encouraging each other. Join the tribe
-                here!
-              </p>
-          </div>
+        <div className="hero-img-container">
+          <img src={coffeeImage} alt="coffee/kamal-preet" className="hero-img" style={{ backgroundColor: "#ced4da", maxWidth: "80%", height: "auto" }} />
         </div>
+          <div className="content">
+            <div className="inner-content">
+              <h1>&#x1F343; Welcome to my blog</h1>
+                <p>
+                  Hey there, fellow health enthusiasts!
+                  Welcome to my vibrant corner of the internet
+                  where I share my thoughts on various topics related to food and wellness. Grab a cup of your favorite herbal tea, kick off those shoes, and get cozy because we're about to
+                  embark on an exciting adventure together!
+
+                  Don't forget to sign up for my newsletter where you'll be the first to hear about
+                  my newest blogs when they drop!
+                </p>
+            </div>
+          </div>
       </div>
+      <hr></hr>
       
 
       <div className="blog-section">
         <div id="blogs">
-          <h4 className="text-danger">Recent Posts</h4>
+          <h4>Recent Posts</h4>
           <ul className='blog-posts'>
             <li>
               <a href="/blog1" className="blog-post">Importance of Fasting</a>
