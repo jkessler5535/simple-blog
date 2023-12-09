25b8d38a916b027bd605e42caa4323005551f861
JamieKessler <jkessler5535@gmail.com>
2023-12-09 11:28:39 -0800

    added Blog.js, applied background and font changes

--- blog/src/components/Blog1.js
+++ blog/src/components/Blog1.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import './Blog1.css';
 
 const Blog1 = () => {
   return (
-    <div>
-      <h1>My First BLog</h1>
-      <p>If the "siteMetadata" field is indeed available on the "Query" type, then the error may be due to incorrect syntax in your query. Make sure that you are using the correct field names and query structure according to the GraphQL specification.</p>
-    </div>
+  <div className="blog-content">
+    <h1>Fasting and Intermittent Fasting</h1>
+
+    <p>Fasting, a centuries-old practice, has proven to be highly effective in enhancing overall health and well-being. One popular approach to fasting is intermittent fasting, which involves periods of intentional food abstention throughout the day or week.</p>
+
+    <h2>Methods of Intermittent Fasting</h2>
+
+    <ul>
+      <li>
+        <strong>Alternate-day fasting:</strong> This method involves eating a normal diet on one day and either completely fasting or having a small meal (less than 500 calories) on the next day.
+      </li>
+      <li>
+        <strong>5:2 fasting:</strong> With this approach, you follow a normal diet for five days of the week and then fast for two days. On the fasting days, you reduce your calorie intake.
+      </li>
+      <li>
+        <strong>Daily time-restricted fasting:</strong> This method involves eating your meals within a specific time window each day. For example, you could skip breakfast and eat lunch around noon, with your last meal of the day by 8 p.m.
+      </li>
+    </ul>
+
+    <h2>Weight Loss and Health Benefits</h2>
+
+    <p>Some studies suggest that alternate-day fasting can be just as effective for weight loss as a typical low-calorie diet. It makes sense since reducing your calorie intake can help with weight loss.</p>
+
+    <p>Curious about the potential health benefits of intermittent fasting? As it turns out, besides aiding in weight loss and promoting physical activity, intermittent fasting may also have positive effects on various health conditions associated with inflammation.</p>
+
+    <p>Research indicates that intermittent fasting could be particularly advantageous for reducing inflammation-related conditions, including but not limited to:</p>
+
+    <ul>
+      <li>Alzheimer's disease</li>
+      <li>Arthritis</li>
+      <li>Asthma</li>
+      <li>Multiple sclerosis</li>
+      <li>Stroke</li>
+    </ul>
+
+    <h2>Potential Side Effects</h2>
+
+    <p>While intermittent fasting can have its benefits, it's essential to be aware of potential side effects, though they often subside within a month. These side effects may include:</p>
+
+    <ul>
+      <li>Hunger</li>
+      <li>Fatigue</li>
+      <li>Insomnia</li>
+      <li>Nausea</li>
+      <li>Headaches</li>
+    </ul>
+
+    <p>Intermittent fasting can be a safe practice for many individuals; however, it may not be suitable for everyone. If you are pregnant or breastfeeding, skipping meals may not be the ideal method for managing your weight. Additionally, if you have medical conditions such as kidney stones, gastroesophageal reflux, or diabetes, it is crucial to consult with your doctor before beginning intermittent fasting.</p>
+  </div>
   );
 }
 
