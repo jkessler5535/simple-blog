8ccd942c94f103191c470481402db2ac0cc7c12a
JamieKessler <jkessler5535@gmail.com>
2023-12-16 17:04:26 -0800

    added blog4.js file & file path on app.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import Blog1 from "./components/Blog1.js";
 import Blog2 from "./components/Blog2.js";
 import Blog3 from "./components/Blog3.js";
+import Blog4 from "./components/Blog4.js";
 import Home from "./components/Home.js";
 import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar.js';
@@ -21,6 +22,7 @@ const App = () => {
         <Route path ='/blog1' element={<Blog1/>} />
         <Route path ='/blog2' element={<Blog2/>} />
         <Route path ='/blog3' element={<Blog3/>} />
+        <Route path ='/blog4' element={<Blog4/>} />
       </Routes>  
       <Footer/>
     </div>
